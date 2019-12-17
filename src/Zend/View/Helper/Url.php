56e95932c75408a84296a6b16f5ee7d89913cb59
Jay Klehr <jay@diablomedia.com>
2019-12-17 08:01:42 -0700

    testing against php 7.4 final, locking dev deps, fixing phpstan errors

--- src/Zend/View/Helper/Url.php
+++ src/Zend/View/Helper/Url.php
@@ -38,6 +38,7 @@ class Zend_View_Helper_Url extends Zend_View_Helper_Abstract
      * @param  array $urlOptions Options passed to the assemble method of the Route object.
      * @param  mixed $name The name of a Route to use. If null it will use the current Route
      * @param  bool $reset Whether or not to reset the route defaults with those provided
+     * @param  bool $encode
      * @return string Url for the link href attribute.
      */
     public function url(array $urlOptions = array(), $name = null, $reset = false, $encode = true)
