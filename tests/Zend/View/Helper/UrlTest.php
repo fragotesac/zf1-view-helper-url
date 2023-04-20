2e169891b8b89402ec54cda64eb3bc1d1ad73c4f
Jay Klehr <jay@diablomedia.com>
2023-04-20 13:56:07 -0600

    Preparing for PHP 8.2 (#226
    * Preparing for PHP 8.2
    
    * final deps

--- tests/Zend/View/Helper/UrlTest.php
+++ tests/Zend/View/Helper/UrlTest.php
@@ -36,6 +36,9 @@
  */
 class Zend_View_Helper_UrlTest extends PHPUnit\Framework\TestCase
 {
+    protected $front;
+    protected $helper;
+
     /**
      * Sets up the fixture, for example, open a network connection.
      * This method is called before a test is executed.
